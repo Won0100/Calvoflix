1c3c4b872d33b88ae0298e5cc91231ee108efa20
Wesley.Nascimento <wesley.nascimento@wittel.com>
2023-11-01 00:07:33 -0300

    adicionando o footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header> </Header>
+      <Header />
       <h1> Hello world</h1>
+      <Footer />
     </>
     );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return(
+        <footer className={styles.footer}>
+            <span>Site criado e desenvolvido por Wesley Nascimento 2023</span>
+        </footer>
+    )
+}
+
+export default Footer
